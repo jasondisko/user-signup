4aa728555432a60bd3c199bfcc30e5984c0cc5c7
jasondisko <jasondisko@gmail.com>
2017-04-10 13:58:09 -0500

    finished user-signup

--- main.py
+++ main.py
@@ -16,10 +16,141 @@
 #
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+import re
+import os
+import jinja2
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+
+#page_header = """
+#<!DOCTYPE html>
+#<html>
+#<head>
+#    <title>Signup</title>
+#    <style type="text/css">
+#        .error {
+#            color: red;
+#        }
+#    </style>
+#</head>
+#<body>
+#    <h1>
+#        Signup
+#    </h1>
+#"""
+
+# html boilerplate for the bottom of every page
+#page_footer = """
+#</body>
+#</html>
+#"""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
+
+
+#class Signup(webapp2.RequestHandler):
+#    def get(self):
+
+#        add_form = """
+#        <form action="/" method="post">
+#            <label>
+#                Username
+#                <input type="text" name="username"/>
+#            </label>
+#
+#            <br>
+#
+#
+#            <br>
+#
+#            <label>
+#                Verify Password
+#                <input type="password" name="verify" value = ""/>
+#            </label>
+#
+#            <br>
+#            Email(Optional)
+    #            <input type="email" name="email"/>
+    #        </label>
+    #        <br>
+    #        <input type="submit" value="Submit"/>
+    #    </form>
+    #    """
+
+
+    #    content = page_header + add_form + page_footer
+    #    self.response.write(content)
+class Signup(BaseHandler):
+
+    def get(self):
+        self.render("signup-form.html")
+
+    def post(self):
+        have_error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify_password = self.request.get("verify")
+        email = self.request.get("email")
+
+        params = dict(username = username, email = email)
+
+
+
+        if not valid_username(username):
+            params['error_username'] = "That's not a valid username"
+            have_error = True
+
+            if not valid_password(password):
+                params['error_password']= "That wasn't a valid password"
+                have_error = True
+            elif password != verify_password:
+                params['error_verify'] = "You're passwords don't match"
+                have_error = True
+
+            if not valid_email(email):
+                params['error_email'] = "That's not a valid email"
+                have_error = True
+
+            if have_error:
+                self.render('signup-form.html', **params)
+            else:
+                self.redirect('/welcome')
+
+
+class Welcome(BaseHandler):
+    def get(self):
+        username = self.request.get('username')
+        if valid_username(username):
+            self.render('welcome.html', username = username)
+        else:
+            self.redirect('/')
+
+
+app = webapp2.WSGIApplication([('/', Signup),
+                            ('/welcome', Welcome)],
+                             debug=True)
